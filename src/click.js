7111838d3b4cd0c99eeb67bd995ad6df11380cd5
winnie_on <winnie.trap@gmail.com>
2018-07-18 06:28:40 +0900

    test setTimeout

---
+++ src/click.js
@@ -0,0 +1,14 @@
+var timerId;
+function clik(){
+  timerId = setTimeout(whattime, 3000);
+}
+function whattime(){
+  document.getElementById("info").textContent = new Date();
+}
+// var timerId;
+// function timeoutStart(){
+//   timerId = setTimeout(whattime, 3000);
+// }
+// function whattime(){
+//   document.getElementById("info").textContent = new Date();
+// }
